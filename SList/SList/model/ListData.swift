2214936a9a5b81c4e56ce1e4a465f734950ae9e8
vineel <vineelgolla@gmail.com>
2020-07-29 22:14:03 -0500

    list data modal

--- SList/SList/model/ListData.swift
+++ SList/SList/model/ListData.swift
@@ -7,3 +7,20 @@
 //
 
 import Foundation
+import MapKit
+
+struct ListData {
+    let listName: String
+    let quantityvalue: Int?
+    let order: Int
+    let Date: Date
+    var mark: Bool
+    
+    init( listname: String, quantityvalue: Int, order: Int, date: Date, mark: Bool) {
+        self.listName = listname
+        self.quantityvalue = quantityvalue
+        self.order = order
+        self.Date = date
+        self.mark = mark
+    }
+}

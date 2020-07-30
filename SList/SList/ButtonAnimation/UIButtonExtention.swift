c27a9481ae289a9b339d306062c1739c34f4f62f
vineel <vineelgolla@gmail.com>
2020-07-29 22:13:28 -0500

    button animation

--- SList/SList/ButtonAnimation/UIButtonExtention.swift
+++ SList/SList/ButtonAnimation/UIButtonExtention.swift
@@ -7,3 +7,41 @@
 //
 
 import Foundation
+import UIKit
+
+extension UIButton{
+    
+
+    
+    func pulsate() {
+        
+        let pulse = CASpringAnimation(keyPath: "transform.scale")
+        pulse.duration = 0.6
+        pulse.fromValue = 0.95
+        pulse.toValue = 1.0
+        pulse.autoreverses = true
+        pulse.repeatCount = 1
+        pulse.initialVelocity = 0.5
+        pulse.damping = 1.0
+        
+        layer.add(pulse, forKey: nil)
+    }
+    
+    func showAnimation() {
+        UIView.animate(withDuration: 0.1,
+                       delay: 0,
+                       options: .curveLinear,
+                       animations: { [weak self] in
+                            self?.transform = CGAffineTransform.init(scaleX: 0.95, y: 0.95)
+        }) {  (done) in
+            UIView.animate(withDuration: 0.1,
+                           delay: 0,
+                           options: .curveLinear,
+                           animations: { [weak self] in
+                                self?.transform = CGAffineTransform.init(scaleX: 1, y: 1)
+            }) 
+        }
+    }
+    
+
+}
